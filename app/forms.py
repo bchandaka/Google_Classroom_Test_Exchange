3021ed1100d740d1a86a2fdfdf41c30acb4cbb10
bchandaka <bhargav2900@gmail.com>
2018-06-10 16:31:19 -0500

    linked db with forms

--- app/forms.py
+++ app/forms.py
@@ -1,17 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import PasswordField, StringField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from app.models import User
 
 class SignupForm(FlaskForm):
     firstname = StringField('First Name', validators=[DataRequired()])
     lastname = StringField('Last Name' , validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     #temp
-    password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = StringField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
-
+from flask import render_template, request, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user
+from werkzeug.urls import url_parse
+from app import db
+from app.models import User
 from app.forms import LoginForm, SignupForm
 
 @app.route('/index')
@@ -9,16 +12,35 @@ def index():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     loginForm = LoginForm()
     if loginForm.validate_on_submit():
-        flash('Login requested from user: {}, remember_me: {}'.format(loginForm.email.data, loginForm.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(email=loginForm.email.data).first()
+        if user is None or not user.check_password(loginForm.password.data):
+            flash('Invalid usernmae or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=loginForm.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title = 'Login', form = loginForm)
-
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 @app.route('/signup', methods = ['GET', 'POST'])
 def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     signupForm = SignupForm()
     if signupForm.validate_on_submit():
-        flash('Signup requested from user: {} {}, email: {}'.format(signupForm.firstname.data, signupForm.lastname.data, signupForm.email.data))
-        return redirect(url_for('index'))
+        user = User(firstname=signupForm.firstname.data, lastname=signupForm.lastname.data, email=signupForm.email.data)
+
+        user.set_password(signupForm.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
     return render_template('signup.html', title = 'Login', form = signupForm)
