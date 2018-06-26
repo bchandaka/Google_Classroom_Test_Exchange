2c16a44faad4c897f9faeee69b830b28588a5c4e
bchandaka <bhargav2900@gmail.com>
2018-06-26 16:58:04 -0500

    Supports Google sheet input and DB has data

--- app/models.py
+++ app/models.py
@@ -2,23 +2,40 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
-
+import datetime
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     firstname = db.Column(db.String(64), index = True)
     lastname = db.Column(db.String(64), index = True)
     email = db.Column(db.String(64), index = True)
     grade = db.Column(db.Integer, index = True)
-    username = db.Column(db.String, index=True)
+    username = db.Column(db.String, index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    def __repr__():
+    last_seen = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    admin = db.Column(db.Boolean, default=False, index=True)
+    def __repr__(self):
         return('<User {}>'.format(self.username))
     def set_password(self,password):
         self.password_hash = generate_password_hash(password)
     def check_password(self,password):
         return check_password_hash(self.password_hash, password)
     def fullname(self):
-        return repr(self.firstname, self.lastname)
+        return '{} {}'.format(self.firstname, self.lastname)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    date = db.Column(db.DateTime, index=True, default=datetime.date.today().strftime("%m/%d/%y"))
+    tournament = db.Column(db.String(64), index = True)
+    team = db.Column(db.String(64), index = True)
+    event_name = db.Column(db.String(64), index = True)
+    #user ids
+    user1_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    user2_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    user3_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    user = db.relationship("User", foreign_keys = [user1_id])
+    def __repr__(self):
+        return('<{} {}>'.format(self.tournament, self.event))
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
---
+++ app/sheets.py
@@ -0,0 +1,43 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from app import db
+from app.models import User, Event
+import datetime
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+tournament = 'State'
+date = datetime.date(2018,5,28)
+sheet = client.open('{} {}'.format(tournament, date)).get_worksheet(1)
+
+def add_user(name):
+    firstname, lastname = name.split()
+    username = firstname[0] + lastname
+    email = 'bhargav2900@gmail.com'
+    password = 'abc' # same password for all fake users
+    grade = 10
+    a = User(firstname = firstname, lastname = lastname, grade = grade, email = email, username = username.lower())
+    a.set_password(password)
+    db.session.add(a)
+    db.session.commit()
+
+def add_event(event_name, name1, name2, name3):
+    user_ids = []
+    for i in [name1,name2,name3]:
+        if i != '':
+            firstname, lastname = i.split()
+            u = User.query.filter_by(firstname=firstname, lastname=lastname).first()
+            user_ids.append(u.id)
+        else:
+            user_ids.append(None)
+
+    global date
+    global tournament
+    global sheet
+    event = Event(date=date, tournament=tournament, team=sheet.title, event_name = event_name, user1_id = user_ids[0], user2_id = user_ids[1], user3_id = user_ids[2])
+    db.session.add(event)
+    db.session.commit()
+
+
